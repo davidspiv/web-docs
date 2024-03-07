bbc8be38d11466d3e647a7302ceb566e2f7e2b7f
davidspiv <daspivack@gmail.com>
2024-03-06 21:03:20 -0800

    init

---
+++ color.js
@@ -0,0 +1,6 @@
+const gridElements = document.querySelectorAll(".grid-element");
+let colors = ["#B1CEBD", "#94AC9E", "#B8D6C5", "#D9926F", "#D6BEAE", "#DBCAAB"];
+for (let i = 0; i < gridElements.length; i++) {
+  const color = colors[Math.floor(Math.random() * colors.length)];
+  gridElements[i].setAttribute("style", `background-color: ${color}`);
+}
